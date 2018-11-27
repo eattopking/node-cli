95a3ce895b58dbc3067686d4266624277e971dbd
zhanghengh <zhanghengh@yonyou.com>
2018-11-27 23:17:49 +0800

    更新依赖

---
+++ src/bin/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const ora = require("ora");
+const logSymbols = require("log-symbols");
+const download = require("download-git-repo");
+const program = require("commander");
+
