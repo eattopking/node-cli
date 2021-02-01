0126d9c32c8bc1de102bfc0296171d1cb88d5e56
zhangheng05 <zhangheng05@baijiahulian.com>
2021-02-02 00:23:27 +0800

    更新

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,29 +1,56 @@
 #!/usr/bin/env node
-// 设置终端输出的字体颜色
+// node 既可以写服务， 也可以写脚本， 所有手动在终点运行和完成的指令， 通过node环境的js代码都可以实现
+// process.exit(1); 这就是在代码中以错误状态退出进程
+
+// 设置终端输出的字体颜色的库
 const chalk = require("chalk");
-// 设置终端输出的loadding的图标和字体
+// node读写文件的模块， 读写文件的时候， 默认路径为指令执行的目录， 所有调用读写文件的时候， 直接写要操作的目录下的文件名就行辣
+const fs = require("fs");
+// 设置终端输出的loadding的过程和图标的库
 const ora = require("ora");
-// 设置终端输出的图标
+// 在node终端输出表格的库， 根据文档在终端输出表格
+const cliTable = require("cli-table");
+// 设置node终端输出的图标的库, 就像这样使用， 设置一个错误的图标， 和一串终端中红色的字
+//console.error(symbols.error,
+// chalk.red(`${err} download template failed, please check your network connection and try again!`)
+// );
 const logSymbols = require("log-symbols");
-// 下载git代码的
+// 下载git上代码的库， download下载git的代码默认下载在指令脚本的目录下
 const download = require("download-git-repo");
-// node.js命令行指令解析库
+// 比较好用的模板库，和node和配， fs读到的文件内容， 转换为字符串，handlebars直接就能使用拉
+//const packageMeta = {
+//   projectName,
+//   description,
+//   author,
+// };
+// const fileName = `${projectName}/package.json`;
+// const content = fs.readFileSync(fileName).toString();
+// const result = handlebars.compile(content)(packageMeta);
+const handlebars = require("handlebars");
+// node.js命令行指令解析库， 可以分别设置多个指令
 const program = require("commander");
-// node.js命令行界面交互库
+// node.js命令行界面交互库, 先给交互设置对应步骤， 最后将交互输入的结果， 一起处理
 const inquirer = require("inquirer");
 // npm 操作文件和目录的
 const { ensureFile, outputFile } = require("fs-extra");
 
+// commander 可以单独设置监听指令， 并且我们就是需要怎么使用他们， 这样可以分模块， 逻辑更清晰
+// 每个指令中不是所有的设置项都要设置一遍， 而是根据需要， 每个指令中设置不同的配置项， 也不是每个指令中都需要设置action配置想，
+// 不需要设置触发什么动作的的时候就不要设置
 program
-  .version("1.0.0", "-v, --version", "version")
-  .usage("[options] <file ...>")
-  .description("react-cli")
-  .alias("xf")
-  .option("-s, --show <show>")
+  .alias("a")
+  .command("add")
+  .action(cmd => {
+    createProject();
+  });
+
+  program
+  .alias("i")
   .command("init")
   .action(cmd => {
     createProject();
   });
+
 program.parse(process.argv);
 
 function createProject() {
